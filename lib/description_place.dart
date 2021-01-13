686efac75dfd434f6da64a1dd95f1ef2e54a403e
raag <raagpc@disroot.org>
2021-01-12 23:46:37 -0600

    params

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,48 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class DescriptionPlace extends StatelessWidget {
-	@override
-	Widget build (BuildContext context) {
 
-		final star = Container(
-			margin: EdgeInsets.only(
-				top: 323.0,
-				right: 3.0,
-			),
-			child: Icon(
-				Icons.star,
-				color: Color(0xFFF2C611)
-			)
-		);
+  String namePlace;
+  int stars;
+  String descriptionPlace;
 
-		final titleStars = Row(
-			children: <Widget>[
-				Container(
-					margin: EdgeInsets.only(
-						top: 320.0,
-						left: 20.0,
-						right: 20.0
-					),
-					child: Text(
-						"Duwili Ella",
-						style: TextStyle(
-							fontSize: 30.0,
-							fontWeight: FontWeight.w900
-						),
-						textAlign: TextAlign.left
-					)
-				),
-				Row(
-					children: [
-						star,
-						star,
-						star,
-						star,
-						star
-					]
-				)	
-			],
-		);
-		return titleStars;
-	}
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final starHalf = Container(
+        margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0,
+        ),
+        child: Icon(Icons.star_half, color: Color(0xFFF2C611))
+    );
+
+    final starBorder = Container(
+        margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0,
+        ),
+        child: Icon(Icons.star_border, color: Color(0xFFF2C611))
+    );
+
+    final star = Container(
+        margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0,
+        ),
+        child: Icon(Icons.star, color: Color(0xFFF2C611))
+    );
+
+    final description = Container(
+      margin: new EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: new Text(
+        descriptionPlace,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+        ),
+      ),
+    );
+
+    final titleStars = Row(
+      children: <Widget>[
+        Container(
+            margin: EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0),
+            child: Text(namePlace,
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+                textAlign: TextAlign.left)),
+        Row(children: [star, star, star, starHalf, starBorder])
+      ],
+    );
+    return Column(
+      children: [titleStars, description],
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  String descriptionDummy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-				appBar: AppBar(
-					title: Text("¡Hola mundo!")
-				),
-				body: new DescriptionPlace()
-			)
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+            appBar: AppBar(title: Text("Hello world")),
+            body: new DescriptionPlace("Bahamas", 4, descriptionDummy))
     );
   }
 }
-
